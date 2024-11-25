66d8e1a3f4be24049c5421c2797099750cfb244c
new commit <ebhos4real@yahoo.com>
2024-11-25 16:30:20 -0500

    learning teer
    commit deployment
    commit now
    ok
    aa

--- expense-v1/servers.tf
+++ expense-v1/servers.tf
@@ -5,4 +5,19 @@ resource "aws_instance" "frontend" {
   tags = {
     Name = "frontend"
   }
-}
+  resource "aws_instance" "backend" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+
+    tags = {
+      Name = "backend"
+    }
+  }
+  }resource "aws_instance" "mysql" {
+    ami           = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+
+    tags = {
+      Name = "mysql"
+    }
+  }
