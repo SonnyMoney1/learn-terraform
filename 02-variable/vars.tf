fc6427ea42f0e8d3c23c34d94bf11e6811859185
new commit <ebhos4real@yahoo.com>
2024-11-27 14:36:08 -0500

    learning teer
    commit deployment
    commit now
    ok
    aa

--- 02-variable/vars.tf
+++ 02-variable/vars.tf
@@ -5,3 +5,22 @@ variable "x" {
 output "x" {
   value = "var.x"
 }
+
+# List Variable
+
+variable "I" {
+  default = [
+  10,
+  "hello",
+  true
+  ]
+}
+
+output "I" {
+  value = "var.I"
+}
+
+# second value in list
+output "I2" {
+  value = var.I[I]
+}
