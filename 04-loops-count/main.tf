f49a1ca5437d0c4faf526ad1fb39eccd9ae4c9f7
new commit <ebhos4real@yahoo.com>
2024-12-09 15:14:09 -0500

    learning the concept within commit Amended
    commit deployment
    commit now
    ok
    aa

---
+++ 04-loops-count/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "ec2-nodes" {
+  count = 3
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-05bd88f36ad2a6107"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+variable "components" {
+  default = ["frontend", "backend", "mysql"]
+}
