4193cf8bb77b14132f296bb8829f0442f352d2ca
new commit <ebhos4real@yahoo.com>
2024-12-19 23:58:28 -0500

    learning the concept within commit Amended ass addd the new conf add
    commit deployment
    commit now
    ok
    aa

---
+++ expense-v3/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "ec2-nodes" {
+  for_each = var.components
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = ["sg-05bd88f36ad2a610"]
+
+    tags = {
+      name = each.key
+     }
+    }
+
+    variable "component" {
+      default = {
+        frontend = {
+          instance_type = "t3.small"
+        }
+        backend = {
+          instance_type = "t3.small"
+        }
+        mysql = {
+          instance_type = "t3.micro"
+      }
+    }
+ }
