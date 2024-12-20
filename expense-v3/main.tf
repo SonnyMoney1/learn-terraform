1e2aabacc5427572bcb5f0371cd3b43e27a4e1b0
new commit <ebhos4real@yahoo.com>
2024-12-20 01:31:48 -0500

    learning the concept within commit Amended ass addd the new conf add
    commit deployment
    commit now
    aa
    ok
    aa

--- expense-v3/main.tf
+++ expense-v3/main.tf
@@ -1,24 +1,36 @@
 resource "aws_instance" "ec2-nodes" {
-  for_each  = var.components
-  ami           = "ami-09c813fb71547fc4f"
-  instance_type = each.value["instance_type"]
+  for_each               = var.components
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = each.value["instance_type"]
   vpc_security_group_ids = ["sg-05bd88f36ad2a610"]
 
-    tags = {
-      name = each.key
-     }
+  tags = {
+    name = each.key
+  }
+}
+
+variable "components" {
+  default = {
+    frontend = {
+      instance_type = "t3.small"
+    }
+    backend = {
+      instance_type = "t3.small"
+    }
+    mysql = {
+      instance_type = "t3.micro"
+    }
+  }
+}
+
+
+
+
+
+
     }
 
-    variable "components {
-      default = {
-        frontend = {
-          instance_type = "t3.small"
-        }
-        backend = {
-          instance_type = "t3.small"
-        }
-        mysql = {
-          instance_type = "t3.micro"
-      }
-    }
- }
+  }
+
+  }
+   { name = each.key }
