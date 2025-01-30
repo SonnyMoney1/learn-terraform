6497d64831cdc95bcfb156da5b4369d13a24fa7a
new commit <ebhos4real@yahoo.com>
2025-01-30 09:46:51 -0500

    learning the concept within commit Amended ass addd the new conf add
    commit deployment
    commit now
    aa
    ok
    aa

--- expense-v5/variable.tf
+++ expense-v5/variable.tf
@@ -1,21 +1,20 @@
-variable "components" {
+variable "component" {
   default = {
     frontend = {
-      instance_type = "t3.small"
+      instance_type= "t3.small"
     }
-    backend {
-      instance_type "t3.small"
+    backend = {
+      instance_type= "t3.small"
     }
-    msql = {
+    mysql = {
       instance_type = "t3.micro"
     }
   }
 }
 
 variable "awi" {
-  default = ami default =
+  default = ami default = "ami-09c813fb71547fc4f"
 }
-variable "vpc_security" {}
-default = {}
-
+variable "vpc_security_group_ids" {
+default =  ["sg-05bd88f36ad2a6107"]
 }
