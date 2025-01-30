5d75ae4d9419d6c09ff5dbc5dc82e73212c77f18
new commit <ebhos4real@yahoo.com>
2025-01-30 11:15:52 -0500

    learning the concept within commit Amended ass addd the new conf add
    commit deployment
    commit now
    aa
    ok
    aa

--- expense-v5/main.tf
+++ expense-v5/main.tf
@@ -1,11 +1,11 @@
-module "app" {
-  for_each = var.components
-  source = "./app"
-  ami                    = var.ami
-  instance_type          = each.value["instance_type"]
-  name                   = each.key
-  vpc_security_group_ids = var.vpc_security_group_ids
-}
+#module "app" {
+ # for_each = var.components
+  #source = "./app"
+  #ami                    = var.ami
+  #instance_type          = each.value["instance_type"]
+  #name                   = each.key
+  #vpc_security_group_ids = var.vpc_security_group_ids
+#}
 
 
 # frontend = {
