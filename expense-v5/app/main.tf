4da68265cf12a5fc1583cf311980ce8061ae3b37
new commit <ebhos4real@yahoo.com>
2025-05-15 22:02:30 -0400

    learning the concept within commit Amended ass addd the new conf add
    commit deployment
    commit now
    aa
    ok
    aa

--- expense-v5/app/main.tf
+++ expense-v5/app/main.tf
@@ -8,6 +8,20 @@ resource "aws_instance" "node" {
   }
 }
 
+resource "aws_route53_record" "record" {
+    zone_id = var.zone_id
+    name = "${var.name}.dev.rdevopsb79.online"
+    type = "A"
+    ttl  =  30
+records = [aws_instance.node.private_ip]
+}
 
+resource "null_resource" "provisioner" {
+    depebds_on = [aws_route53_record.record]
+    provisioner "local.exec" {
+        command = "sleep 120; cd /home/ec2-user/expense-ansible ; ansible-playbook -i ${aws_instance.node.private_ip}, -e ansible_user=ec2-user -e
+        ansible_password=DevOps321 -e role_name=${var.name} -e env=dev expense.yml"
+        }
+    }
 
 
